79b19d3b8cb01616307ff80bd027486d166270d0
Đình Trí <106405422+masontris@users.noreply.github.com>
2024-03-01 23:55:59 +0700

    controller login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,60 @@
-const User = require('../models/user');
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-exports.registerUser = (req, res) => {
-    console.log(req.body);
+exports.registerUser = async (req, res) => {
+  try {
     const { name, email, password, image } = req.body;
-    const newUser = new User({ name, email, password, image });
-    newUser.save()
-        .then(() => {
-            res.status(200).json({ message: "Đăng ký người dùng thành công!" });
-        })
-        .catch((err) => {
-            if (err.code === 11000 && err.keyPattern && err.keyPattern.email === 1) {
-                // Trường hợp trùng lặp email
-                console.log("Email đã tồn tại trong cơ sở dữ liệu");
-                return res.status(400).json({ message: "Email này đã được dùng!" });
-            }
-            console.log("Error registering user", err);
-            res.status(500).json({ message: "Đăng ký người dùng thất bại!" });
-        });
+
+    if (!name || !email || !password || !image) {
+      return res.status(400).json({ message: "Cần điền đầy đủ thông tin!" });
+    }
+    // Hash mật khẩu
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Tạo User
+    const newUser = new User({ name, email, password: hashedPassword, image });
+    await newUser.save();
+
+    res.status(200).json({ message: "Đăng ký người dùng thành công!" });
+  } catch (err) {
+    // Trường hợp trùng lặp email
+    if (err.code === 11000 && err.keyPattern && err.keyPattern.email === 1) {
+      console.log("Email đã tồn tại trong cơ sở dữ liệu");
+      return res.status(400).json({ message: "Email này đã được dùng!" });
+    }
+    console.log("Lỗi hàm registerUser", err);
+    res.status(500).json({ message: "Đăng ký người dùng thất bại!" });
+  }
+};
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Cần có email và mật khẩu!" });
+    }
+    //Tìm người dùng trong database
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Người dùng không tồn tại!" });
+    }
+    //So sánh mật khẩu sau khi lấy user lên
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch == false) {
+      return res.status(401).json({ message: "Mật khẩu không chính xác!" });
+    }
+
+    // Nếu mọi thứ đều chính xác, trả về phản hồi thành công và tạo token
+    const token = createToken(user._id);
+    return res.status(200).json({ message: "Đăng nhập thành công!", token });
+  } catch (err) {
+    console.error("Error logging in user", err);
+    return res.status(500).json({ message: "Đã xảy ra lỗi khi đăng nhập!" });
+  }
+};
+const createToken = (userId) => {
+  const payload = {
+    userId: userId,
+  };
+  const token = jwt.sign(payload, "your_secret_key", { expiresIn: "1h" });
+  return token;
 };
--- backend/routers/userRoute.js
+++ backend/routers/userRoute.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const { registerUser } = require("../controllers/userController");
+const { loginUser } = require("../controllers/userController");
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+
 router.get("/abc",(req, res)=>{
     res.send("hello");
 } );
