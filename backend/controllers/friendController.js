4aab981a32d0e89868d19ee83105db3f835a23ca
Đình Trí <106405422+masontris@users.noreply.github.com>
2024-03-07 04:10:25 +0700

    controller get list sent and request friend

--- backend/controllers/friendController.js
+++ backend/controllers/friendController.js
@@ -1,26 +1,55 @@
 const User = require("../models/user");
 
+//Kết bạn
 exports.addFriend = async (req, res) => {
   const { currentUserId, selectedUserId } = req.body;
-    
-//   const jsonString = JSON.stringify(currentUserId);
-
-  console.log(req.body);
-
   try {
     //update của người nhận lời mời
     await User.findByIdAndUpdate(selectedUserId, {
       $push: { friendRequests: currentUserId },
     });
-
     //update của người gửi lời mời
     await User.findByIdAndUpdate(currentUserId, {
       $push: { sentFriendRequests: selectedUserId },
     });
 
-    return res.status(200).json({message: "Gửi lời mời thành công"});
+    return res.status(200).json({ message: "Gửi lời mời thành công" });
   } catch (error) {
     console.error("Error retrieving users", err);
-     res.status(200).json({message: "Gửi lời mời thất bại"});
+    res.status(200).json({ message: "Gửi lời mời thất bại" });
+  }
+};
+//Lấy tất cả lời mời kết bạn đã gửi
+exports.getListFriendRequests = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    //
+    const user = await User.findById(userId)
+      .populate("friendRequests", "name email image") //Điền các trường name email của người vào freindRequests
+      .lean();
+
+    const freindRequests = user.friendRequests;
+
+    res.json(freindRequests);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+//Lấy tất cả lời mời đã nhận
+exports.getListSentFriendRequests = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId)
+      .populate("sentFriendRequests", "name email image")
+      .lean();
+
+    const sentFriendRequests = user.sentFriendRequests;
+
+    res.json(sentFriendRequests);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ error: "Internal Server" });
   }
 };
--- backend/routers/friendRouter.js
+++ backend/routers/friendRouter.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var router = express.Router();
-const { addFriend } = require("../controllers/friendController");
+const { addFriend, getListFriendRequests, getListSentFriendRequests } = require("../controllers/friendController");
+
 
 router.post("/friend-request", addFriend);
+router.get("/getListFriendRequest/:userId", getListFriendRequests);
+router.get("/getListSentFriendRequests/:userId", getListSentFriendRequests);
 
 module.exports = router;
